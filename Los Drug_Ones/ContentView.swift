d38568dbdac2d01f72aed5a6b182939b55ea62db
abigaildemicco <abdemicco@gmail.com>
2022-02-10 22:13:09 +0100

    Initial Commit

---
+++ Los Drug_Ones/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Los Drug_Ones
+//
+//  Created by Abigail De Micco on 10/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Los Drug_Ones/Los_Drug_OnesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Los_Drug_OnesApp.swift
+//  Los Drug_Ones
+//
+//  Created by Abigail De Micco on 10/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Los_Drug_OnesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
