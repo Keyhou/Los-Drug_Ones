5436464a9105bc15c134d9e769701a4db70ab174
Keyhou <31210145+Keyhou@users.noreply.github.com>
2022-02-14 13:00:51 +0100

    Creating files

---
+++ Los Drug_Ones/View/ScheduleView.swift
@@ -0,0 +1,20 @@
+//
+//  ScheduleView.swift
+//  Los Drug_Ones
+//
+//  Created by Keyhan Mortezaeifar on 14/02/22.
+//
+
+import SwiftUI
+
+struct ScheduleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScheduleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScheduleView()
+    }
+}
